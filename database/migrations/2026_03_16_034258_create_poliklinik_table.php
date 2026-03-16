1ed659d7302a11d2da0d4a1f7a36cabba3f2e742
rimuren <shadqi1890@gmail.com>
2026-03-16 11:03:03 +0700

    Membuat tabel parent

---
+++ database/migrations/2026_03_16_034258_create_poliklinik_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('poliklinik', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nama', 100);
+            $table->string('kode_bpjs', 20)->nullable();
+
+            $table->string('image')->nullable();
+            $table->string('background_img')->nullable();
+
+            $table->integer('tarif_konsultasi')->default(0);
+            $table->integer('jumlah_dokter')->default(0);
+
+            $table->boolean('senin')->default(false);
+            $table->boolean('selasa')->default(false);
+            $table->boolean('rabu')->default(false);
+            $table->boolean('kamis')->default(false);
+            $table->boolean('jumat')->default(false);
+            $table->boolean('sabtu')->default(false);
+            $table->boolean('minggu')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('poliklinik');
+    }
+};
