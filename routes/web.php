2a13ce99b3dcdfbd203f10592d0df58122f4e242
rimuren <shadqi1890@gmail.com>
2026-04-01 10:36:48 +0700

    Perubahan di routes/web.php dan .env.example

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DokterController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 use App\Services\RsudApiService;
 
@@ -10,7 +11,7 @@ Route::get('/guest/index', function () {
     return view('guest.index');
 })->name('guest.index');;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'permission:admin-access'])->group(function () {
     Route::view('/admin/dashboard', 'admin.dashboard')->name('admin.dashboard');
 
     Route::view('/admin/artikel/index','admin.artikel.index')->name('admin.artikel.index');
@@ -18,10 +19,14 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('/admin/artikel/kategori/index','admin.artikel.kategori.index')->name('admin.artikel.kategori.index');
 
     Route::view('/admin/akun', 'admin.akun.index')->name('admin.akun.index');
-    
-    Route::view('/admin/akun/role', 'admin.akun.role.index')->name('admin.akun.role.index');
 
- // Route::view('/admin/dokter', 'admin.dokter.index')->name('admin.dokter.index');
+    Route::get('/admin/akun/role/create', [RoleController::class, 'create'])->name('admin.akun.role.create');
+    Route::post('/admin/akun/role', [RoleController::class, 'store'])->name('admin.akun.role.store');
+    Route::get('/admin/akun/role/{role}/edit', [RoleController::class, 'edit'])->name('admin.akun.role.edit');
+    Route::put('/admin/akun/role/{role}', [RoleController::class, 'update'])->name('admin.akun.role.update');
+    Route::delete('/admin/akun/role/{role}', [RoleController::class, 'destroy'])->name('admin.akun.role.destroy');
+    Route::get('/admin/akun/role', [RoleController::class, 'index'])->name('admin.akun.role.index');
+
     Route::get('/admin/dokter', [DokterController::class, 'index'])->name('admin.dokter.index');
 
     Route::view('/admin/dokumentasi/foto', 'admin.dokumentasi.foto.index')->name('admin.dokumentasi.foto');
