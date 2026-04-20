0905952a201d9ee12cfec339300f7a8f8e3a6628
rimuren <shadqi1890@gmail.com>
2026-04-20 13:37:32 +0700

    update: migration, doktertable, poliklinik_table, jadwal_dokter

--- database/migrations/2026_03_25_010711_create_jadwal_dokter.php
+++ database/migrations/2026_03_25_010711_create_jadwal_dokter.php
@@ -13,18 +13,20 @@ return new class extends Migration
     {
         Schema::create('jadwal_dokter', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('api_id')->nullable()->unique()->index();
             $table->unsignedBigInteger('dokter_id')->nullable();
-            $table->unsignedBigInteger('poliklinik_id')->nullable();
+            $table->unsignedBigInteger('poliklinik_id')->nullable()->index();
             $table->unsignedBigInteger('ruangan_id')->nullable();
+            $table->string('ruangan_nama')->nullable();
             $table->string('kode_jadwal', 20)->nullable();
-            $table->enum('hari', ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'])->nullable();
+            $table->enum('hari', ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'])->nullable()->index();
             $table->integer('hari_order')->nullable();
             $table->time('jam_mulai')->nullable();
             $table->time('jam_selesai')->nullable();
-            $table->string('tipe_pelayanan', 10)->nullable();
+            $table->string('tipe_pelayanan', 50)->nullable();
             $table->timestamps();
-
             $table->foreign('dokter_id')->references('id')->on('dokter')->onDelete('cascade');
+            $table->foreign('poliklinik_id')->references('id')->on('poliklinik')->nullOnDelete();
         });
     }
 
